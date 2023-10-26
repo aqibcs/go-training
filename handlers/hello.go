6d5b24d67159b87e43df2905840241526692db8e
aqibcs <aqibshahcs@gmail.com>
2023-10-26 11:56:35 +0500

    Fixed Bugs In Files

--- handlers/hello.go
+++ handlers/hello.go
@@ -1,25 +1,24 @@
 package handlers
 
 import (
+	"encoding/json"
 	"go-training/models/request"
 	"go-training/models/response"
-	"encoding/json"
 	"net/http"
 	"time"
 )
 
-
 func HelloHandler(w http.ResponseWriter, r *http.Request) {
-	var RequestBody request.RequestBody
+	var requestBody request.RequestBody
 	decoder := json.NewDecoder(r.Body)
-	err := decoder.Decode(&RequestBody)
+	err := decoder.Decode(&requestBody)
 	if err != nil {
 		http.Error(w, "Inavlid requestbody", http.StatusBadRequest)
 	}
 
-	response := response.ResponseBody {
-		Code: 200,
-		Message: "Welcome " + RequestBody.Name + "!",
+	response := response.ResponseBody{
+		Code:      200,
+		Message:   "Welcome " + requestBody.Name + "!",
 		Timestamp: time.Now().UTC().Format(time.RFC3339),
 	}
 
