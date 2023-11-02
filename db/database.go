0d0318288461f6c9676940f529e2698074b36d8c
aqibcs <aqibshahcs@gmail.com>
2023-11-02 16:05:02 +0500

    Add Function to database.go

--- db/database.go
+++ db/database.go
@@ -3,27 +3,38 @@ package db
 import (
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
-	"log"
+	"sync"
 )
 
-var DB *gorm.DB
+var (
+	dB      *gorm.DB
+	dbMutex sync.Mutex
+)
+
+func GetDB() *gorm.DB {
+	dbMutex.Lock()
+	defer dbMutex.Unlock()
+	return dB
+}
 
 func Init() {
 	// postgres://<user_name>:<password>@localhost:<port>/<database_name>
 	dsn := "postgres://myuser:mypassword@localhost:5432/mydatabase"
 	var err error
-	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	dB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic("Failed to connect to the database")
 	}
+}
 
-	// Ensure the database connection is valid
-	postgresDB, err := DB.DB()
+func PingDB() error {
+	postgresDB, err := dB.DB()
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 	err = postgresDB.Ping()
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
+	return nil
 }
