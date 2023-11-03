deb23ed5590379ba173e81c017314b7a01f65028
aqibcs <aqibshahcs@gmail.com>
2023-11-03 11:23:49 +0500

    Removed Redundency

--- db/database.go
+++ db/database.go
@@ -3,38 +3,31 @@ package db
 import (
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
-	"sync"
 )
 
 var (
-	dB      *gorm.DB
-	dbMutex sync.Mutex
+	conn *gorm.DB
 )
 
 func GetDB() *gorm.DB {
-	dbMutex.Lock()
-	defer dbMutex.Unlock()
-	return dB
+	return conn
 }
 
 func Init() {
 	// postgres://<user_name>:<password>@localhost:<port>/<database_name>
 	dsn := "postgres://myuser:mypassword@localhost:5432/mydatabase"
 	var err error
-	dB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	conn, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic("Failed to connect to the database")
 	}
 }
 
 func PingDB() error {
-	postgresDB, err := dB.DB()
+	postgresDB, err := conn.DB()
 	if err != nil {
 		return err
 	}
-	err = postgresDB.Ping()
-	if err != nil {
-		return err
-	}
-	return nil
+
+	return postgresDB.Ping()
 }
