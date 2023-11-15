ebd3e681437dd89bc7d3f8f00ba325f522aa0bac
aqibcs <aqibshahcs@gmail.com>
2023-11-15 16:54:28 +0500

    Code Refactoring

--- config/env.go
+++ config/env.go
@@ -1,29 +1,43 @@
 package config
 
 import (
-	"github.com/joho/godotenv"
 	"log"
 	"os"
+
+	"github.com/joho/godotenv"
 )
 
 var (
-	Username     string
-	Password     string
-	DatabaseName string
-	Host         string
-	Port         string
+	Username     = "myuser"
+	Password     = ""
+	DatabaseName = "mydatabase"
+	Host         = "localhost"
+	Port         = "5432"
 )
 
-func LoadEnv() {
-	err := godotenv.Load()
+func init() {
+	err := godotenv.Load(".env")
 	if err != nil {
-		log.Fatal("Error loading .env file")
+		log.Fatalf("Error loading .env file")
+	  }
+
+	if x := os.Getenv("POSTGRES_USER"); x != "" {
+		Username = x
 	}
 
-	// Get environment variables for database connection
-	Username = os.Getenv("POSTGRES_USER")
-	Password = os.Getenv("POSTGRES_PASSWORD")
-	DatabaseName = os.Getenv("POSTGRES_DB")
-	Host = os.Getenv("POSTGRES_HOST")
-	Port = os.Getenv("POSTGRES_PORT")
+	if x := os.Getenv("POSTGRES_PASSWORD"); x != "" {
+		Password = x
+	}
+
+	if x := os.Getenv("POSTGRES_DB"); x != "" {
+		DatabaseName = x
+	}
+
+	if x := os.Getenv("POSTGRES_HOST"); x != "" {
+		Host = x
+	}
+
+	if x := os.Getenv("POSTGRES_PORT"); x != "" {
+		Port = x
+	}
 }
